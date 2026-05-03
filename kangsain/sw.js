c0c5a8491b94e1cf141aa6f58a985069f3a77b43
archivepilates <home@archivepilates.com>
2026-05-03 10:59:41 +0900

    Add KangsaIN service worker for PWA installability

---
+++ kangsain/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'archive-kangsain-shell-v6';
+const APP_SHELL = [
+  './',
+  './manifest.webmanifest',
+  './icons/icon-192.png?v=6',
+  './icons/icon-512.png?v=6',
+  './icons/apple-touch-icon.png?v=6'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin || event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
